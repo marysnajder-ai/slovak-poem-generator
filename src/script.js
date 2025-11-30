7760d84177ae2406d13bb167c2a54ea08ee9837d
marysnajder-ai <maryam.snajder@gmail.com>
2025-11-30 15:33:53 +0100

    updated JS

---
+++ src/script.js
@@ -0,0 +1,28 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "7d9d8aed460317f0t10f235204bb13o9";
+  let context =
+    "You are a nature poem expert. Write a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a Slovak poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Slovak poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
